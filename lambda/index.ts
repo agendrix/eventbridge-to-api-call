8d40ab40ce1b16714cfebed9387d5f5e2b764923
Claude-GP <c.garrison-pelletier@agendrix.com>
2024-01-29 09:27:44 -0500

    fix ci

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,6 +1,7 @@
 import { Handler } from "aws-lambda";
 import axios from 'axios';
 import { Payload, Text } from "./types";
+import { error } from "console";
 
 const handler: Handler = async (payload: Payload) => {
   const apiKey = process.env.API_Key;
@@ -8,6 +9,10 @@ const handler: Handler = async (payload: Payload) => {
   const { headers, data } = payload;
 
   try {
+    if (!apiUrl)
+    {
+        throw error;
+    }
     // Make the HTTP request using Axios
     const response = await axios.post(apiUrl, data, {
         headers: headers,
--- test/mocks/index.ts
+++ test/mocks/index.ts
@@ -1,1 +1,22 @@
-import "./libs/aws-sdk";
+export const PAYLOAD = {
+  headers: [
+    
+  ],
+  data: {
+    "message": "An example alert message",
+    "alias": "Life is too short for no alias",
+    "description":"Every alert needs a description",
+    "responders":[
+        {"id":"4513b7ea-3b91-438f-b7e4-e3e54af9147c", "type":"team"},
+    ],
+    "visibleTo":[
+        {"id":"4513b7ea-3b91-438f-b7e4-e3e54af9147c","type":"team"},
+    ],
+    "actions": ["Restart", "AnExampleAction"],
+    "tags": ["OverwriteQuietHours","Critical"],
+    "details":{"key1":"value1","key2":"value2"},
+    "entity":"An example entity",
+    "priority":"P1"
+  }
+} 
+
